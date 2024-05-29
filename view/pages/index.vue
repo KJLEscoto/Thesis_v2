1a7a049a07778092b5c834f461c4b6c0e42bf26b
Kent Joemar L. Escoto <kentescoto24@gmail.com>
2024-05-29 18:35:15 +0800

    update-backend

--- view/pages/index.vue
+++ view/pages/index.vue
@@ -4,8 +4,7 @@
     <div class="lg:grid-cols-2 grid-cols-1 lg:grid flex flex-col-reverse lg:h-screen h-auto w-auto lg:gap-20 gap-10 lg:p-20 p-16">
       <section class="m-auto">
         <h1 class="font-bold cursor-default lg:text-4xl text-3xl lg:text-start text-center">Empower Your Security</h1>
-        <p class="mt-5 cursor-default lg:text-start text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut laudantium aliquam beatae. Debitis accusamus aperiam odio repudiandae ea quaerat illo facilis fugiat nobis? Possimus temporibus, sed unde placeat consectetur quae?
-          A, ut consequatur. Debitis iste repudiandae explicabo sit nemo natus molestiae optio neque aperiam, itaque cum illo vel quae incidunt quis deleniti ad earum provident quibusdam. Nobis odit consequuntur sint.</p>
+ 
         <div class="flex lg:justify-start justify-center mt-7">
           <UButton 
             :loading="loading" 
